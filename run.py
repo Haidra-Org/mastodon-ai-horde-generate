b5a34e360a26fa8d2492aaf497cc47f1ce597a33
db0 <mail@dbzer0.com>
2024-11-15 14:57:26 +0100

    test

--- run.py
+++ run.py
@@ -25,8 +25,6 @@ def init_mastodon():
     for notification in notifications:
         if db_r.get(str(notification["id"])):
             continue
-        logger.info(notification['created_at'])
-        continue
         notification_handler = MentionHandler(notification)
         thread = threading.Thread(target=notification_handler.handle_notification, args=())
         thread.start()
