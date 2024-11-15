dd8924647f95fbae84ce7f2aa15fef221363bbcb
db0 <mail@dbzer0.com>
2024-11-15 15:09:41 +0100

    test

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def init_mastodon():
     for notification in notifications:
         if db_r.get(str(notification["id"])):
             continue
-        if notification['created_at'] < datetime.now(timezone.utc) - timedelta(minutes=3):
+        if notification['created_at'] < datetime.now(timezone.utc) - timedelta(hours=3):
             logger.debug(f"Init notification {notification['id']} is more than 3 hours old. Ignoring")
             continue
         notification_handler = MentionHandler(notification)
